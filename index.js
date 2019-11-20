61e304604f1419cd960df86a1634bf0e4b0e3bc7
pavellll002 <53902665+pavellll002@users.noreply.github.com>
2019-11-20 19:17:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const request = require('request')
+const fs 		= require('fs')
+
+
+let check = ()=>{
+	//get file
+	request('https://chatvdvoem.ru/',(err,res,body)=>{
+
+		let str = body//body file
+		//number of online
+		let online = str.match(/<strong id="online_counter">(\d*)<\/strong>/)
+
+		//file for saving online
+		let statistic = fs.readFileSync('statistic.json','utf-8'),
+		date = new Date(),
+		day = date.getDay(),
+		hour = date.getHours(),
+		days = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб']
+		
+
+		statistic =	JSON.parse(statistic)
+		//add online
+		statistic[days[day]][hour].push(online[1])
+		
+		statistic = JSON.stringify(statistic)
+		//save online to the file
+		fs.writeFileSync('statistic.json',statistic)
+
+
+	})
+}
+
+setInterval(check, 1000*60*10)
